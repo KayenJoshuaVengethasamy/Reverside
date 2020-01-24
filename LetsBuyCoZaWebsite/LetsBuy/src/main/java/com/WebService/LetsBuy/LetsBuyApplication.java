33882788bbf705c539d2824178f4544945831541
KayenJoshuaVengethasamy <60103904+KayenJoshuaVengethasamy@users.noreply.github.com>
2020-01-24 15:05:42 +0200

    First two user stories
    Register User
    Login User with registered credentials

---
+++ LetsBuyCoZaWebsite/LetsBuy/src/main/java/com/WebService/LetsBuy/LetsBuyApplication.java
@@ -0,0 +1,13 @@
+package com.WebService.LetsBuy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LetsBuyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LetsBuyApplication.class, args);
+	}
+
+}
---
+++ LetsBuyCoZaWebsite/LetsBuy/src/main/java/com/WebService/LetsBuy/service/UserServiceImpl.java
@@ -0,0 +1,56 @@
+package com.WebService.LetsBuy.service;
+
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.stereotype.Service;
+
+import com.WebService.LetsBuy.model.Role;
+import com.WebService.LetsBuy.model.User;
+import com.WebService.LetsBuy.repository.UserRepository;
+import com.WebService.LetsBuy.web.dto.UserRegistrationDto;
+
+@Service
+public class UserServiceImpl implements UserService {
+
+	@Autowired
+	private UserRepository userRepository;
+
+	@Autowired
+	private BCryptPasswordEncoder passwordEncoder;
+
+	public User findByUserEmail(String userEmail) {
+		return userRepository.findByUserEmail(userEmail);
+	}
+
+	public User save(UserRegistrationDto registration) {
+		User user = new User();
+		user.setUserFirstName(registration.getFirstName());
+		user.setUserLastName(registration.getLastName());
+		user.setUserEmail(registration.getEmail());
+		user.setUserPassword(passwordEncoder.encode(registration.getPassword()));
+		user.setRoles(Arrays.asList(new Role("ROLE_USER")));
+		return userRepository.save(user);
+	}
+
+	@Override
+	public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+		User user = userRepository.findByUserEmail(email);
+		if (user == null) {
+			throw new UsernameNotFoundException("Invalid username or password.");
+		}
+		return new org.springframework.security.core.userdetails.User(user.getUserEmail(), user.getUserPassword(),
+				mapRolesToAuthorities(user.getRoles()));
+	}
+
+	private Collection<? extends GrantedAuthority> mapRolesToAuthorities(Collection<Role> roles) {
+		return roles.stream().map(role -> new SimpleGrantedAuthority(role.getRoleName())).collect(Collectors.toList());
+	}
+}
